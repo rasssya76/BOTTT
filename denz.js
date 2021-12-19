fc9d002886cbfc102ac484146365277ab4aa4001
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2021-12-20 05:08:16 +0700

    Add files via upload

--- denz.js
+++ denz.js
@@ -1181,9 +1181,7 @@ _*${sym}━━「 MENU OWNER 」*_
  _${rm} ${prefix}gemuk [ _reply audio_ ]_
  _${rm} ${prefix}detikvn [ _reply audio caption angka_ ]_
  _${rm} ${prefix}detikvideo [ _reply video caption angka_ ]`
-sendButDocument(from, `${menu}`, "C R E A T E B Y R A M A G A N S",  fs.readFileSync('./sampah/Denpa'), {mimetype:Mimetype.pdf, thumbnail:fs.readFileSync('./media/image/banner.jpg'), filename:`${jmn} - ${week} - ${calender}`}, [{buttonId:``,buttonText:{displayText:'S&k \n\nSebenar nya saya itu. suka ngegay diam diam kalo ada yg minat ngegay sama saya silahkan pc ya muach😘😘'},type:1}], {quoted:fmen, contextInfo: { mentionedJid: [dtod,otod,stod], forwardingScore: 508, isForwarded: true, externalAdReply:{title:`${tampilUcapan} ${pushname}`,body:`*click here to play music`,mediaType:"2",thumbnail:ofrply,mediaUrl:`https://youtu.be/uQiF1yOnzDg`}}})
-lima = fs.readFileSync('./help.mp3');
-denz.sendMessage(from, lima, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
+sendButDocument(from, `${menu}`, "C R E A T E B Y R A M A G A N S",  fs.readFileSync('./sampah/Denpa'), {mimetype:Mimetype.pdf, thumbnail:fs.readFileSync('./media/image/banner.jpg'), filename:`${jmn} - ${week} - ${calender}`}, [{buttonId:``,buttonText:{displayText:'S&k \n\nSebenar nya saya itu. suka ngegay diam diam kalo ada yg minat ngegay sama saya silahkan pc ya muach😘😘'},type:1}], {quoted:fmen, contextInfo: { mentionedJid: [dtod,otod,stod], forwardingScore: 508, isForwarded: true, externalAdReply:{title:`${tampilUcapan} ${pushname}`,body:`*click here to play music`,mediaType:"2",thumbnail:ofrply,mediaUrl:`https://youtu.be/OwwHPRIju60`}}})
 break
 case 'command':
  stod = `${sender}`
